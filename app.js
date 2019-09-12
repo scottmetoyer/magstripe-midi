1d63e700a431d244c6f1c3be549472a5ef8eebef
Scott Metoyer <scott.metoyer@gmail.com>
2019-09-12 13:14:25 -0700

    Added polyrhythm swipe sequence mode

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const output = new midi.Output();
 var portCount = output.getPortCount();
 const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 
-var notes = [];
-var currentStep = 0;
-var maxSteps = 6;
-var stepDelay = 250;
+var stepDelay = 1000;
 const midiEvent = 144;
 
 console.log("Available MIDI devices:");
@@ -17,7 +14,7 @@ for (var i = 0; i < portCount; i++) {
 }
 
 // Set the appropriate device number here
-var deviceNumber = 2;
+var deviceNumber = 1;
 output.openPort(deviceNumber);
 
 console.log('Opened ' + output.getPortName(deviceNumber) +  '. Waiting for input.');
@@ -28,34 +25,37 @@ const input = readline.createInterface({
 });
 
 input.on('line', (input) =>{
-  console.log('Received input');
-
   // Parse the input into a set of notes
   // 144 is channel 1 out
   // Map from C2 - C6
-  notes[0] = Math.round(scale(input.substr(4, 2), 0, 100, 48, 84));
-  notes[1] = Math.round(scale(input.substr(5, 2), 0, 100, 48, 84));
-  notes[2] = Math.round(scale(input.substr(6, 2), 0, 100, 48, 84));
-  notes[3] = Math.round(scale(input.substr(7, 2), 0, 100, 48, 84));
-  notes[4] = Math.round(scale(input.substr(8, 2), 0, 100, 48, 84));
-  notes[5] = Math.round(scale(input.substr(9, 2), 0, 100, 48, 84));
+  var seq = {};
+  seq.notes = [];
+  seq.step = 0;
 
-  console.log('1:' + notes[0] + ' 2:' + notes[1] + ' 3:' + notes[2] + ' 4:' + notes[3]);
+  // Create a sequence between 3 and 6 steps long, fill it up with mapped midi data from the scan
+  seq.length = getRandom(3, 7);
+
+  for (var x = 0; x < seq.length; x++) {
+    seq.notes[x] = Math.round(scale(input.substr(x + 4, 2), 0, 100, 48, 84));
+  }
+
+  start(seq);
 });
 
-const start = async () => {
-  await asyncLoop(notes, async (num) => {
+const start = async (seq) => {
+  console.log('Starting sequence:');
+  console.log(seq);
+
+  await asyncLoop(seq, async (note, step) => {
     await waitFor(stepDelay);
 
-    if (notes[currentStep]) {
+    if (note) {
       // Play note stored at the position in the array
-      console.log('Play step ' + currentStep + ': ' + notes[currentStep]);
+      console.log('Playing step ' + step + ': ' + note);
 
-      output.sendMessage([midiEvent, notes[currentStep], 127]);
-      var note = notes[currentStep];
-      setTimeout(noteOff, 10, note);
+      output.sendMessage([midiEvent, note, 127]);
+      setTimeout(noteOff, 5, note); // 5 millisecond delay? Not sure if this is acceptable.
     }
-    // console.log(num);
   });
 }
 
@@ -63,22 +63,26 @@ function noteOff(note) {
   output.sendMessage([128, note, 0]);
 }
 
-
 // output.sendMessage([176,22,1]);
 // output.closePort();
+
+// Return a random integer between min (inclusive) and max (exclusive)
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-async function asyncLoop(array, callback) {
+async function asyncLoop(seq, callback) {
   do {
-    await callback(array[currentStep], currentStep, notes);
-    currentStep += 1;
-    if (currentStep == maxSteps) {
-      currentStep = 0;
+    await callback(seq.notes[seq.step], seq.step);
+    seq.step += 1;
+    if (seq.step == seq.length) {
+      seq.step = 0;
     }
   } while (true);
 }
-
-start();
