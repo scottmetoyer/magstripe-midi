f89281e5016296b5d53a682b851c6b2f15139eda
Scott Metoyer <scott.metoyer@gmail.com>
2019-09-13 13:01:36 -0700

    Added MIDI clock sync

--- app.js
+++ app.js
@@ -1,40 +1,73 @@
 const midi = require('midi');
 const readline = require('readline');
 
-const output = new midi.Output();
-var portCount = output.getPortCount();
-const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+var outputMidiDeviceNumber = 0;
+var inputMidiDeviceNumber = 1;
 
+const midiOutput = new midi.Output();
+const midiInput = new midi.Input();
+const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
 var stepDelay = 1000;
+var isPlaying = false;
+var lastClockPulse = new Date();
 const midiEvent = 144;
 
-console.log("Available MIDI devices:");
-for (var i = 0; i < portCount; i++) {
-  console.log(i + ': ' + output.getPortName(i));
-}
+console.log("Available MIDI output devices:");
+enumerateMidiDevices(midiOutput);
+console.log("Available MIDI input devices:");
+enumerateMidiDevices(midiInput);
 
-// Set the appropriate device number here
-var deviceNumber = 2;
-output.openPort(deviceNumber);
+// Initialize MIDI inputs and outputs
+midiOutput.openPort(outputMidiDeviceNumber);
+midiInput.openPort(inputMidiDeviceNumber);
 
-console.log('Opened ' + output.getPortName(deviceNumber) +  '. Waiting for input.');
+// Enable MIDI clock
+midiInput.ignoreTypes(true, false, true);
 
-const input = readline.createInterface({
+console.log('Opened ' + midiOutput.getPortName(outputMidiDeviceNumber) +  ' for output.');
+console.log('Opened ' + midiInput.getPortName(inputMidiDeviceNumber) +  ' for input.');
+
+// Configure a MIDI input callback
+midiInput.on('message', (deltaTime, message) => {
+  // The message is an array of numbers corresponding to the MIDI bytes:
+  //   [status, data1, data2]
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+  // information interpreting the messages.
+  // console.log(`m: ${message} d: ${deltaTime}`);
+
+  // Handle MIDI clock
+  if (message == 248) {
+    var currentTime = new Date();
+    var elapsed = currentTime - lastClockPulse;
+    stepDelay = elapsed * 24; // Multiply by 24 to get quarter note time from ppqn
+    lastClockPulse = currentTime;
+  }
+
+  // Handle start and continue
+  if (message == 250 || message == 251) {
+    console.log('Sequencer is playing')
+    isPlaying = true;
+  }
+
+  // Handle stop
+  if (message == 252) {
+    console.log('Sequencer is stopped')
+    isPlaying = false;
+  }
+});
+
+const consoleInput = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-input.on('line', (input) =>{
+consoleInput.on('line', (input) =>{
   // Parse the input into a set of notes
   // 144 is channel 1 out
   // Map from C2 - C6
-  var seq = {};
-  seq.notes = [];
-  seq.step = 0;
+  var seq = initSequence();
 
-  // Create a sequence between 3 and 6 steps long, fill it up with mapped midi data from the scan
-  seq.length = getRandom(3, 7);
-
+  // Fill the sequence with notes mapped from the input string
   for (var x = 0; x < seq.length; x++) {
     seq.notes[x] = Math.round(scale(input.substr(x + 4, 2), 0, 100, 48, 84));
   }
@@ -49,20 +82,39 @@ const start = async (seq) => {
   await asyncLoop(seq, async (note, step) => {
     await waitFor(stepDelay);
 
-    if (note) {
+    if (note && isPlaying) {
       // Play note stored at the position in the array
       console.log('Playing step ' + step + ': ' + note);
 
-      output.sendMessage([midiEvent, note, 127]);
-      setTimeout(noteOff, 10, note); // 5 millisecond delay? Not sure if this is acceptable.
+      midiOutput.sendMessage([midiEvent, note, 127]);
+      // setTimeout(noteOff, 10, note); // 5 millisecond delay? Not sure if this is acceptable.
     }
   });
 }
 
-function noteOff(note) {
-  output.sendMessage([128, note, 0]);
+function enumerateMidiDevices(device) {
+  var portCount = device.getPortCount();
+
+  for (var i = 0; i < portCount; i++) {
+    console.log(i + ': ' + device.getPortName(i));
+  }
 }
 
+function noteOff(note) {
+  midiOutput.sendMessage([128, note, 0]);
+}
+
+function initSequence() {
+  var seq = {};
+  seq.notes = [];
+  seq.step = 0;
+  seq.probability = 100;
+
+  // Create a sequence between 3 and 6 steps long, fill it up with mapped midi data from the scan
+  seq.length = getRandom(3, 7);
+
+  return seq;
+}
 // output.sendMessage([176,22,1]);
 // output.closePort();
 
